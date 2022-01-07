7ce3198263ba74914880c1ac8088c6e335d99c35
boofi <boofi@hrzn.mail>
2022-01-07 20:14:03 +0100

    added a bunch of cool shit :sunglasses:

--- js/pfp.js
+++ js/pfp.js
@@ -16,8 +16,23 @@ let getPage = function (url, callback) {
 
 let pfp = function () {
     getPage("https://discordid.eu/api?key=did_BGE2SMMVa5M73qTLHB2FIXGuARgMPJI5niwmemGSoZsQeMCcNZyl4iAdlNqz&id=784101881567379456", function (data) {
-        let json = JSON.parse(data);
+        let datajson = JSON.parse(data);
         //set img source of pfp
-        document.getElementById("avatarimg").src = json.data.avatar;
+        console.log("Ignore the error above <33, its expected behaviour");
+
+        //set the src attribute of avatarimg to the url of the pfp
+        $("img.avatarimg").attr("src", datajson.avatar + "?size=1024");
+        console.log("pfp set");
+        document.getElementById("usernameh1").innerHTML = datajson.usernamewtag;
+
     })
+}
+
+let CopyUsername = function () {
+    let copyText = document.getElementById("usernameh1").innerHTML;
+    navigator.clipboard.writeText(copyText);
+    alert("Copied username to clipboard!");
+}
+let openPfp = function () {
+    window.open($("img.avatarimg").attr("src"), "_blank");
 }
